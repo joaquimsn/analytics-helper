02040574f9b00ebe1ac01dbab9309b9a51c3a6dc
Joaquim Neto <jsn.piata@gmail.com>
2018-11-29 12:23:54 -0200

    test(text): commit para teste

--- test/jsdom-site-mock.js
+++ test/jsdom-site-mock.js
@@ -3,6 +3,6 @@ const jsdom = require('jsdom'),
 const virtualConsole = new jsdom.VirtualConsole();
 const { JSDOM } = jsdom;
 
-const dom = new JSDOM(fs.readFileSync('/mnt/LINUX-DATA/workspace/git/javascript/analytics-helper/test/src/site/index.html').toString(), { virtualConsole });
+const dom = {}; //new JSDOM(fs.readFileSync('/mnt/LINUX-DATA/workspace/git/javascript/analytics-helper/test/src/site/index.html').toString(), { virtualConsole });
 
 module.export = dom;
